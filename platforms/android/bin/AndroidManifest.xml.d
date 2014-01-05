83c6fe60b39f91ca506de10cf73d262f2812f36f
thruthesky <thruthesky@gmail.com>
2014-01-05 19:26:50 +0900

    importing default app

---
+++ platforms/android/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+D:\work\www\app\app\platforms\android\bin\AndroidManifest.xml : \
+D:\work\www\app\app\platforms\android\AndroidManifest.xml \
---
+++ platforms/android/bin/AppName-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+D:\work\www\app\app\platforms\android\bin\AppName-debug-unaligned.apk : \
+D:\work\www\app\app\platforms\android\bin\AppName.ap_ \
+D:\work\www\app\app\platforms\android\bin\classes.dex \
+D:\work\www\app\app\platforms\android\CordovaLib\bin\classes.jar \
